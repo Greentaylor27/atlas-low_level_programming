46b5f5e9670c27989eee125a379bd5fd41105731
Greentaylor27 <7196@holbertonstudents.com>
2023-09-20 12:02:06 -0700

    order

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,19 +6,17 @@
  */
 int main()
 {
-	int intType;
-	float floatType;
-	double doubleType;
 	char charType;
+	int intType;
 	long longType;
 	long long longlongType;
+	float floatType;
 
+	printf("Size of char: %zu bytes\n", sizeof(charType));
 	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of long int: %lu bytes\n", sizeof(longType));
+	printf("Size of long long int: %llu byte\n", sizeof(longlongType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
-	printf("Size of long: %zu bytes\n", sizeof(longType));
-	printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
 
 	return (0);
 }
