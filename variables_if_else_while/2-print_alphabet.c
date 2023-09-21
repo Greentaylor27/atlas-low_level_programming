329d05cf60a646e985ba9fbd90c53886ef32f72c
Greentaylor27 <7196@holbertonstudents.com>
2023-09-21 10:00:23 -0700

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints a specific line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char abc = 'a';
+
+	while (abc <= 'z')
+		putchar(abc);
+	abc++
+}
+
+putchar('\n');
+return (0);
