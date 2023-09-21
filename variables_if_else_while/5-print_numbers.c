e9de7b0de360b24a2d31d8c478c7cce084005630
Greentaylor27 <7196@holbertonstudents.com>
2023-09-21 12:07:03 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints a specific line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int t = 0;
+		while (t < 10)
+		{
+			printf("%d", t);
+			t++;
+		}
+	printf("\n");
+	return (0);
+}
