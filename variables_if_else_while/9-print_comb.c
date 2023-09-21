6abb6317da46626235cc87fd16374b9b0d70f3ad
Greentaylor27 <7196@holbertonstudents.com>
2023-09-21 13:51:51 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints a specific line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+		for (i = 0; i <= 9; i++)
+		{
+			for (j = i; j <= 9; j++)
+			{
+				putchar('O' + i);
+				if (i != j)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar('O' + j);
+				}
+				putchar('\n');
+			}
+		}
+		return (0);
+}
