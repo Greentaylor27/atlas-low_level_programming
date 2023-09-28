86e91ad1b9a621395093309aa7ab13bc45bf8e48
Greentaylor27 <7196@holbertonstudents.com>
2023-09-28 09:09:09 -0700

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints a specific line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c[] = "/_putchar"
+	int i
+		for (i = 0; i < 9; i++)
+		{
+			_putchar(c[i]);
+		}
+	_putchar('\n');
+	return (0);
+}
