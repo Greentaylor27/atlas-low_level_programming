9021214088891f691a189ed7117f0c452dd565fb
Greentaylor27 <7196@holbertonstudents.com>
2023-09-28 10:59:52 -0700

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * _isalpha = checks if a character is alphabetical
+ *
+ * return = 1 if true
+ * return = 0 if false
+ */
+
+int _isalpha(int c);
+{
+	int c
+
+
+	if (c <= '97' && c >= '122' && c <= '65' && c >= '90')
+		return (1)
+	else
+		return (0)
+}
