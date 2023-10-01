04bcc20444b3e1f528bde30f87238eddc8790670
Greentaylor27 <7196@holbertonstudents.com>
2023-10-01 07:39:45 -0700

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+void more_numbers(void);
+{
+	int i;
+	int x;
+
+	for (i = '0'; i <= 14; i++)
+	{
+		while (x <= 10)
+		{
+			_putchar(i);
+		}
+		_putchar('\n')
+	}
+}
