2f0bae18f52ff85e8973f6b32c2a7e57d1005601
Greentaylor27 <7196@holbertonstudents.com>
2023-10-01 08:00:23 -0700

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ *
+ * Description: prints a line
+ * @n: number of lines
+ * return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		for (i = 1; 1 <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
+
