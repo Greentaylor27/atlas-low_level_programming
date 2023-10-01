e88c9471034c1f2c6a5047b4aeec9cabd5040459
Greentaylor27 <7196@holbertonstudents.com>
2023-10-01 07:00:57 -0700

    1~

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,14 +3,15 @@
 /**
  * _isdigit - input
  *
- * @c: value
- * return: 1 yes 0 no
+ * Description: define if a character is a number
+ * @c: charater
+ * Return: 1 yes 0 no
  */
 
 int _isdigit(int c)
 {
 	if (c <= 9 && c >= 0)
-		return (1);
-	else
 		return (0);
+	else
+		return (1);
 }
