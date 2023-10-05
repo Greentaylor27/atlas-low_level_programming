d71f2fe040709fab5f2383290e0aba4ff76cf75e
Greentaylor27 <7196@holbertonstudents.com>
2023-10-05 11:13:43 -0700

    @~~~~~

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	for (int i = 0; i < n; i++)
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
 	{
 		dest[i] = src[i];
 	}
-	for (int i = strlen(src); i < n; i++)
+	while (i < n)
 	{
 		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
