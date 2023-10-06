88b5dd45605dd1c29319e86d8507b3095000d506
Greentaylor27 <7196@holbertonstudents.com>
2023-10-06 10:23:50 -0700

    ^^

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes a string
+ * @x: string to change
+ * Return: new string
+ */
+char *cap_string(char *x)
+{
+	int i;
+
+	if (*x == '\0')
+	{
+		return ('\0');
+	}
+	for (i = 0; x[i] != '\0'; i++)
+	{
+		if (x[i] >= 'a' && x[i] <= 'z')
+		{
+			x[i] = x[i] - 32;
+		}
+	}
+	return (x)
