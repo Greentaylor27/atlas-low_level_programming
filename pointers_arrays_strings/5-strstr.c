dffa203fb65de3c187ba0aa99fd1266257303f46
Greentaylor27 <7196@holbertonstudents.com>
2023-10-09 18:25:27 -0700

    567

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: Substring
+ * Return: pointer or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*haystack == '\0' || *needle == '\0')
+	{
+		return ('\0');
+	}
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+
+		haystack++;
+	}
+	return ('\0')
+}
