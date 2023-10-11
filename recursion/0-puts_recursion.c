3460bccaa33066dc0418b0336999d4839d01854f
Greentaylor27 <7196@holbertonstudents.com>
2023-10-11 10:05:43 -0700

    Fixing_Compile_ERROR_3

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -10,10 +10,10 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar("\n");
+		putchar("\n");
 		return;
 	}
 
-	_putchar("%c", *s);
+	putchar("%c", *s);
 	_puts_recursion(s + 1);
 }
