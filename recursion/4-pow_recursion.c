3d39ca5221e3fc4b4b8b40004ebb1d7129f14718
Greentaylor27 <7196@holbertonstudents.com>
2023-10-12 12:55:29 -0700

    POWERvi 4-pow_recursion.c vi 4-pow_recursion.c !

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - does exponent mathmatically
+ * @x: base int
+ * @y: Power!!!
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y != 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+		return (1);
+}
