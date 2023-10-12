52f9acb7a7790d7e9de1e1871bb1f890323bcc2f
Greentaylor27 <7196@holbertonstudents.com>
2023-10-12 12:25:37 -0700

    FACTORIALS\!\!\!\!\!

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - Does Factorials
+ * @n: Number
+ * Return: -1 for error, or gives answer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else (n == 0)
+	{
+		return (1);
+	}
+	else if 
+	{
+		return (n * factorial(n - 1));
+	}
+}
