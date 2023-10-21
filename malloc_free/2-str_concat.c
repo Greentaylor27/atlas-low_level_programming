0af13b2e343bc510b99a3bfe67af7d3829aeabc5
Greentaylor27 <7196@holbertonstudents.com>
2023-10-21 10:00:57 -0700

    First push

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: Pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+	char* result;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	result = (char*)malloc((len1 + len2 + 1) * sizeof(char));
+	strcpy(result, s1);
+	strcat(result, s2);
+
+	return result;
+}
