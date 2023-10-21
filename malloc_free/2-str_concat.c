eb3eca8cd947dc70febc9be9c7636ad9368ab0c7
Greentaylor27 <7196@holbertonstudents.com>
2023-10-21 10:02:57 -0700

    Betty clean up

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 {
 	size_t len1 = strlen(s1);
 	size_t len2 = strlen(s2);
-	char* result;
+	char *result;
 
 	if (s1 == NULL)
 	{
@@ -25,9 +25,9 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 
-	result = (char*)malloc((len1 + len2 + 1) * sizeof(char));
+	result = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	strcpy(result, s1);
 	strcat(result, s2);
 
-	return result;
+	return (result);
 }
