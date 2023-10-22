5c8498643a7ac39609fe73bcfa246f96ece9d6c9
Greentaylor27 <7196@holbertonstudents.com>
2023-10-22 13:39:37 -0700

    First push of project 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: pointer to first string
+ * @s2: pointer to the second string
+ * @n: number of bytes
+ * Return: pointer to new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = strlen(s1);
+	unsigned int len2 = strlen(s2);
+	unsigned int i;
+	char *result;
+
+	result = (char *)malloc((len1 + n + 1) * sizeof(char));
+
+	for (i = 0; i < len1; i++)
+	{
+		result[i] = s1[i];
+	}
+	for (i = 0; i <= n; i++)
+	{
+		result[len1 + i] = s2[i];
+	}
+	result[len1 + n] = '\0';
+	return (result);
+}
