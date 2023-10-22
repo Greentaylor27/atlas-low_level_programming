720d70bbef49f08796bcb74c1c8a2df2798a4f0d
Greentaylor27 <7196@holbertonstudents.com>
2023-10-22 13:53:54 -0700

    final push

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,11 +26,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+	if (n >= len2)
+	{
+		n = len2;
+	}
 	for (i = 0; i < len1; i++)
 	{
 		result[i] = s1[i];
 	}
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		result[len1 + i] = s2[i];
 	}
