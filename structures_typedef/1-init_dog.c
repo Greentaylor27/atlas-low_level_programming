2dec60b64f38a085b59350d6eefeb97aa23c339e
Greentaylor27 <7196@holbertonstudents.com>
2023-10-26 09:43:29 -0700

    Sixth push

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -14,11 +14,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != '\0')
+	if (*d != '\0')
 	{
-	d -> owner = owner;
-	d -> age = age;
-	d -> name = name;
+	d->owner = owner;
+	d->age = age;
+	d->name = name;
 	}
-	return;
 }
