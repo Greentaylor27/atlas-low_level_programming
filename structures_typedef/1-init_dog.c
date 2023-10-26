ff79934077b4cde91ba312a02d0635c49d8aa419
Greentaylor27 <7196@holbertonstudents.com>
2023-10-26 09:27:33 -0700

    Second push

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -15,15 +15,18 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	int i;
-	int j;
 
 	for (i = 0; name[i] != '\0'; i++)
 	{
-		name[i] = name[i];
+		d->name[i] = name[i];
 	}
-	for (j = 0; owner[j] != '\0'; j++)
+	d->name[i] = '\0';
+
+	for (i = 0; owner[i] != '\0'; i++)
 	{
-		owner[i] = owner[i];
+		d->owner[i] = owner[i];
 	}
-	age = age;
+	d->owner[i] = '\0';
+
+	d->age = age;
 }
