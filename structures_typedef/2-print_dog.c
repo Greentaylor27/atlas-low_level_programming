015720a445699b1de32503a6faf4a3576e4610f0
Greentaylor27 <7196@holbertonstudents.com>
2023-10-26 11:27:10 -0700

    Fixing compile error

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,15 +13,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		puts("Name: (nil)\n");
+		putchar("Name: (nil)\n");
 	else
-		puts("Name: %s\n", d->name);
+		putchar("Name: %s\n", d->name);
 	if (d->age == NULL)
-		puts("Age: (nil)\n");
+		putchar("Age: (nil)\n");
 	else
-		puts("Age: %f\n", d->age);
+		putchar("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		puts("Owner: (nil)\n");
+		putchar("Owner: (nil)\n");
 	else
-		puts("Owner: %s\n", d->owner);
+		putchar("Owner: %s\n", d->owner);
 }
