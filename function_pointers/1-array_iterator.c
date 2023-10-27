ea89c577176c6bd4423bab0ca9e51e6c0e19a914
Greentaylor27 <7196@holbertonstudents.com>
2023-10-27 11:32:20 -0700

    First push

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - iterates through an array
+ * @array: points to an array
+ * @size: size of array
+ * @action: function to be call back
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action();
+	}
+}
