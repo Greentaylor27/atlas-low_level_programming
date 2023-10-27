05ee8ba909e9c27b94fbcbae5181d07acbf5ac9f
Greentaylor27 <7196@holbertonstudents.com>
2023-10-27 11:36:11 -0700

    Fixing error

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,6 +14,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL && action == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
