e637eefec9c40401f67936f23949a1a679557526
Greentaylor27 <7196@holbertonstudents.com>
2023-11-10 15:07:19 -0800

    The struggle bus is reallybetty 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,33 +10,12 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode;
-	list_t *current;
+	list_t *newNode = (list_t *c)malloc(sizeof(list_t));
 
-	newNode = malloc(sizeof(list_t));
-	current = malloc(sizeof(list_t));
-
-	if (newNode == NULL)
-	{
-		return (NULL);
-	}
 	newNode->str = strdup(str);
+
 	newNode->len = strlen(str);
-	newNode->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = newNode;
-	}
-	else
-	{
-		current = *head;
-
-		while ((current->next != NULL))
-		{
-			current = current->next;
-		}
-		current->next = newNode;
-	}
+	newNode->next = *head;
+	*head = newNode;
 	return (newNode);
 }
