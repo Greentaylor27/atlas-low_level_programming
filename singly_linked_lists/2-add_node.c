9b375c248ffaf89ab5445c1a94c3488f2e25fdc1
Greentaylor27 <7196@holbertonstudents.com>
2023-11-10 14:33:03 -0800

    first commit

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add_node - adds a node to a singly linked list
+ * @head: a pointer to the head pointer of the linked list.
+ * @str: the string to be stored in the new node.
+ *
+ * Return: a pointer to the newly created node.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	newNode = (struct list_t)malloc(sizeof(list_t));
+
+	if (newNode == NULL)
+		exit(exit_failure);
+	newNode->str = str;
+	newNode->next = NULL;
+	return (newNode);
+}
