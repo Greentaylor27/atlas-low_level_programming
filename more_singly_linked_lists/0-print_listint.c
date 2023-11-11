6fc38b36cbfed79211b14337aaf07739e3c243c4
Greentaylor27 <7196@holbertonstudents.com>
2023-11-11 13:20:04 -0800

    First push

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints a singly linked list
+ * @h: pointer to the head element
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->n != NULL)
+		{
+			printf("[%d]\n", h->n);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
