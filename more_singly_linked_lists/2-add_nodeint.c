f3fa48c0cd87b3f1294980134aeb9f971ec3352a
Greentaylor27 <7196@holbertonstudents.com>
2023-11-11 13:48:24 -0800

    first push

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a node to the start of a singly linked list
+ * @head: a pointer to the head pointer of the linked list
+ * @n: the integer to be stored in the new node
+ * Return: a pointer to the newly created node.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode = (list_t *)malloc(sizeof(list_t));
+
+	if (newNode == 0)
+	{
+		return (1);
+	}
+
+	newNode->n = strdup(n);
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
