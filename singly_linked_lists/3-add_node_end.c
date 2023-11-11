fc90a5ecc86fe15200d85fc67afea12ec7ba6258
Greentaylor27 <7196@holbertonstudents.com>
2023-11-11 12:45:39 -0800

    Fixing case statements

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,22 +10,34 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *last = *head;
-	list_t *newNode = (list_t *)malloc(sizeof(list_t));
+	list_t *newNode, *current;
+	newNode = mallox(sizeof(list_t));
 
 	if (newNode == NULL)
+	{
 		return (NULL);
+	}
 	newNode->str = strdup(str);
+
+	if (newNode->str == NULL)
+	{
+		return (NULL);
+	}
 	newNode->len = strlen(str);
 	newNode->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = newNode;
-		return (NULL);
 	}
-	while (last->next != NULL)
-		last = last->next;
-	last->next = newNode;
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = newNode;
+	}
 	return (newNode);
 }
