b7da8e022d55e3467163cd3c981100d4917144d7
Greentaylor27 <7196@holbertonstudents.com>
2023-11-11 12:47:38 -0800

    fixing typo

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newNode, *current;
 
-	newNode = mallox(sizeof(list_t));
+	newNode = malloc(sizeof(list_t));
 
 	if (newNode == NULL)
 	{
