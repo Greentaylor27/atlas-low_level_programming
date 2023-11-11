f6dad778857bee4777b6d245f98a482616e239bf
Greentaylor27 <7196@holbertonstudents.com>
2023-11-11 15:04:21 -0800

    Fixing compile errors

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,9 +9,9 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	list_t *newNode, *current;
+	listint_t *newNode, *current;
 
-	newNode = malloc(sizeof(list_t));
+	newNode = malloc(sizeof(listint_t));
 
 	if (newNode)
 	{
