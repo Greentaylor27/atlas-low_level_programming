10234447fb91c1bcc610bcaf850c08a7562e9ce8
Greentaylor27 <7196@holbertonstudents.com>
2023-11-13 09:56:28 -0800

    First push

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to an unsigned integer
+ * @b: pointer to string.
+ * Return: the decimal number of a binary number or 0 on fail.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int val = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i] == '0' || b[i] == '1')
+	{
+		val <<= 1;
+		val += b[i] - '0';
+		i++;
+	}
+	return (val);
+}
