324fdf1efd0240c8689d8824ff9a8380d37f9c81
Greentaylor27 <7196@holbertonstudents.com>
2023-11-13 10:29:13 -0800

    Fixing

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,9 +11,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int val = 0;
 	int i = 0;
 
-	if (b == NULL)
+	if (b != NULL)
 		return (0);
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] == '0' && b[i] == '1')
 	{
 		val <<= 1;
 		val += b[i] - '0';
