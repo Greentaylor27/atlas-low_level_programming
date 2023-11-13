64cc9695de1a624f6b266a1eebe5695c31adf7ec
Greentaylor27 <7196@holbertonstudents.com>
2023-11-13 10:32:40 -0800

    fixing case

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,12 +11,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int val = 0;
 	int i = 0;
 
-	if (b != NULL)
+	if (b == NULL)
 		return (0);
-	while (b[i] == '0' && b[i] == '1')
+	while (b[i] != NULL)
 	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 		val <<= 1;
-		val += b[i] - '0';
+		if (b[i] == '1')
+			val |= 1;
 		i++;
 	}
 	return (val);
