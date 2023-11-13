ace0884fae0b083812928af2d7e4cb4fc92470b9
Greentaylor27 <7196@holbertonstudents.com>
2023-11-13 07:37:57 -0800

    First push

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * *get_nodeint_at_index - returns the nth node in a linked list
+ * @head: points to the head element in a list
+ * @index: index to return node location
+ * Return: node at index
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	int count = 0;
+
+	while (current != NULL)
+	{
+		if (count == index)
+		{
+			return (current->n)
+		}
+		count++;
+		current = current->next;
+	}
+}
