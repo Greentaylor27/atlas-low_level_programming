1cf16c193197a11def70b130ffb0f3ed69eb7951
Greentaylor27 <7196@holbertonstudents.com>
2023-11-13 10:20:20 -0800

    added an if statement

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,10 +13,14 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while (b[i] == '0' && b[i] == '1')
+	while (b[i] == '0' || b[i] == '1')
 	{
 		val <<= 1;
 		val += b[i] - '0';
+		if (b[i] != '0' || b[i] != '1')
+		{
+			return (NULL);
+		}
 		i++;
 	}
 	return (val);
