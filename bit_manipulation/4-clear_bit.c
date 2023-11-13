b5d723c70f6b0a84f7b37d335d19c703112e9f6d
Greentaylor27 <7196@holbertonstudents.com>
2023-11-13 13:23:30 -0800

    First push

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets a bit to 0
+ * @n: bit to be manipulated
+ * @index: wherethe bit should be manipulated
+ * Return: 1 on success and -1 on fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	mask = ~(1UL << index);
+
+	*n |= mask;
+	return (1);
+}
