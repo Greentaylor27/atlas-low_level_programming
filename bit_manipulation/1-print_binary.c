342177cd5a734fc14a41f83a9194d20fe7d4e750
Greentaylor27 <7196@holbertonstudents.com>
2023-11-13 11:24:51 -0800

    First push

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a binary number
+ * @n: number to be converted
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int num, i;
+	unsigned long int b;
+	bool first;
+
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	num = sizeof(n) * 8;
+
+	first = false;
+
+	for (i = num - 1; i >= 0; --i)
+	{
+		b == (n >> i) & 1;
+
+		if (b == 1)
+			first = true;
+		if (first)
+			putchar(b = '0');
+	}
+}
+
