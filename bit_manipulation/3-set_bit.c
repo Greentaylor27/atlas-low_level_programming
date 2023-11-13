1d38909da3a7f223f747186986f67e3d691d2a47
Greentaylor27 <7196@holbertonstudents.com>
2023-11-13 13:12:43 -0800

    fixing main block of code and declaring variables

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,8 +9,14 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	*n &= ~(1 << index);
+	
+	mask = 1UL << index;
+
+	*n |= mask;
+
 	return (1);
 }
