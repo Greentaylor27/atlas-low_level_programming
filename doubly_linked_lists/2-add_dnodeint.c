945b9d0d93b5bad1568ff96bd5fe7e8caa75b55e
Greentaylor27 <7196@holbertonstudents.com>
2023-11-18 05:58:18 -0800

    Double dereferencing head at the end

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		newNode->next = (*head);
 		newNode->prev = NULL;
 		if (*head != NULL)
-			head->prev = newNode;
+			(*head)->prev = newNode;
+		*head = newNode;
 	}
 	return (newNode);
 }
