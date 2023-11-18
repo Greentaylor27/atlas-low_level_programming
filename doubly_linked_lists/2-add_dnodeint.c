b632ae78efe0a246e668ac00d90b5b490ca2d427
Greentaylor27 <7196@holbertonstudents.com>
2023-11-18 05:55:17 -0800

    removing * from head in if loop

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		newNode->next = (*head);
 		newNode->prev = NULL;
 		if (*head != NULL)
-			*head->prev = newNode;
+			head->prev = newNode;
 	}
 	return (newNode);
 }
