5eb660f53a5043d96f283545e182f83df6e7bb30
Greentaylor27 <7196@holbertonstudents.com>
2024-04-16 09:47:41 -0500

    Fixing compile issues?

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *current, *newNode;
 	int i;
 
-	if (ht == NULL || key == NULL || *key == '\0'
+	if (ht == NULL || key == NULL || *key == '\0')
 		return 0;
 	i = key_index((unsigned char *)key, ht->size);
 	current = ht->array[i];
