16b577f9a81afd1ad165110d52f71724796ebce4
Greentaylor27 <greentaylor18@gmail.com>
2024-07-11 18:56:36 -0500

    Changing while statement

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 	const dlistint_t *current = h;
 
-	while (count != NULL)
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
