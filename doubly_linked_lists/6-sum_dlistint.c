863ba1adbaebb2934508f6842ddc65ed7f9ad181
Greentaylor27 <greentaylor18@gmail.com>
2024-07-15 15:20:42 +0000

    First attempt at adding nodes together

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+	* sum_dlistint - Adds together nodes in a doubly linked list
+	* @head: points to the head element in a list
+	*
+	*Return: The sum of the list or NULL if empty
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	unsigned int sum = 0;
+
+	if (!head)
+		return (NULL);
+
+	for (current = head; current != NULL; current = current->next)
+		sum = current->n;
+	return(sum);
+}
