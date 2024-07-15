a45b0b715e5cb64388724860af76586a03d1cc70
Greentaylor27 <greentaylor18@gmail.com>
2024-07-15 14:38:17 +0000

    Fixing comment by adding a star

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -4,7 +4,7 @@
 	* add_dnodeint_end - adds a new node to the end of a linked list
 	* @head: reference to the head element of a linked list
 	* @n: an integer to be added to the linked list
-
+	*
 	* Return: appended list or NULL on fail
 */
 
