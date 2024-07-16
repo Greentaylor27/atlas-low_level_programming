ccd487dba611aebcf6438fef4ad9ab36cc3ebbf3
Greentaylor27 <greentaylor18@gmail.com>
2024-07-16 02:52:35 +0000

    First attempt at print hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+	* hash_table_print - prints out key value pairs from a hash table
+	* @ht: The hash table you wish to print from
+	*
+	* Return: Associated key value pair or Nothing
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	printf("{\n");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			printf("'%s': '%s'", current->key, current->value);
+			if (current->next != NULL)
+				printf(", ");
+		}
+	}
+	printf("\n}\n");
+}
