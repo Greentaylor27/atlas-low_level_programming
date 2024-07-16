de78671aebdf043edca51ae625f07aa17096cdb9
Greentaylor27 <greentaylor18@gmail.com>
2024-07-16 01:28:57 +0000

    Key at the index given the size

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+	* key_index - gives you the index of a key
+	* @key: the data stored at the index
+	* @size: size of the array
+	* Return: index of the key where it is stored at
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+	{
+	return (hash_djb2(key) % size);
+	}
